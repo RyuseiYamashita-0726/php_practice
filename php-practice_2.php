7f20e6d6fb04ac0cb3ad148f12cc0e761e22409e
RyuseiYamashita <ryusei.19990726@gmail.com>
2025-03-05 17:39:31 +0900

    練習問題2_Q1

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,6 +1,23 @@
 <?php
 // Q1 tic-tac問題
+function tictac($i){
+    
+    if($i % 4 == 0 and $i % 5 == 0){
+        echo "tictac\n";
+    }elseif($i % 4 == 0){
+        echo "tic\n";
+    }elseif($i % 5 == 0){
+        echo "tac\n";
+    }else{
+        echo "{$i}\n";
+    }
+}
 
+
+
+for($i = 1; $i <= 100; $i++){
+    tictac($i);
+}
 
 // Q2 多次元連想配列
 
