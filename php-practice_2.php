294576325c1f4000e7abf655212327cb03596604
RyuseiYamashita <ryusei.19990726@gmail.com>
2025-03-06 12:23:52 +0900

    練習問題2_Q3

--- php-practice_2.php
+++ php-practice_2.php
@@ -20,6 +20,50 @@ for($i = 1; $i <= 100; $i++){
 }
 
 // Q2 多次元連想配列
+//問題１
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
+
+echo "Bさんの電話番号は{$personalInfos[1]["tel"]}です。";
+
+//問題２
+
+foreach ($personalInfos as $keys => $value){
+    $keys ++;
+    echo "{$keys}番目の{$value['name']}のメールアドレスは{$value['mail']}で、電話番号は{$value['tel']}です。\n";
+}
+
+//問題３
+
+$ageList = [25, 30, 18];
+
+$i = 0;
+$num = 0;
+
+foreach($personalInfos as $keys => $value){
+    
+    $personalInfos[$i]['age'] = $ageList[$num];
+    
+    $i++; 
+    $num++;
+}
+
+var_dump($personalInfos);
 
 
 // Q3 オブジェクト-1
